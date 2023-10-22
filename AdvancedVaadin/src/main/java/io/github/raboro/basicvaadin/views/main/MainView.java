ee3bf0265c44fb16c0018f1b96d52b3192b97153
Raboro <marius.woerfelreal@gmail.com>
2023-10-22 22:43:20 +0200

    added logout button

--- AdvancedVaadin/src/main/java/io/github/raboro/basicvaadin/views/main/MainView.java
+++ AdvancedVaadin/src/main/java/io/github/raboro/basicvaadin/views/main/MainView.java
@@ -7,24 +7,31 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import io.github.raboro.basicvaadin.security.SecurityService;
+import jakarta.annotation.security.PermitAll;
 
+@PermitAll
 @PageTitle("Main")
-@Route(value = "")
+@Route("")
 public class MainView extends HorizontalLayout {
 
+    private final SecurityService securityService;
     private TextField name;
     private Button sayHello;
 
-    public MainView() {
+    public MainView(SecurityService securityService) {
+        this.securityService = securityService;
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
         sayHello.addClickListener(e -> Notification.show("Hello " + name.getValue()));
         sayHello.addClickShortcut(Key.ENTER);
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        Button logout = new Button("Logout", e -> this.securityService.logout());
 
-        add(name, sayHello);
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello, logout);
+
+        add(name, sayHello, logout);
     }
 
 }
