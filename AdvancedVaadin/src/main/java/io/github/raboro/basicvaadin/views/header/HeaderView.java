57e55224a668a8c914249db75b104bdb8a020d0c
Raboro <raborogit@gmail.com>
2023-10-28 13:37:07 +0200

    added and integrated HeaderView

---
+++ AdvancedVaadin/src/main/java/io/github/raboro/basicvaadin/views/header/HeaderView.java
@@ -0,0 +1,37 @@
+package io.github.raboro.basicvaadin.views.header;
+
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import io.github.raboro.basicvaadin.security.SecurityService;
+
+/**
+ * @author MariusWoerfel
+ */
+public class HeaderView extends HorizontalLayout {
+
+    public HeaderView(SecurityService securityService) {
+        constructConfig();
+        addData(securityService);
+    }
+
+    private void constructConfig() {
+        setId("header");
+        setWidthFull();
+        setSpacing(true);
+        setAlignItems(Alignment.CENTER);
+    }
+
+    private void addData(SecurityService securityService) {
+        H1 title = new H1("Overview");
+        title.getStyle().set("margin-left", "43%");
+        Button logoutButton = new Button("Logout", e -> securityService.logout());
+        logoutButton.getStyle().set("margin-left", "auto");
+        logoutButton.getStyle().set("margin-right", "3%");
+        Avatar avatar = new Avatar(securityService.getAuthenticatedUser().getUsername());
+        avatar.getStyle().set("margin-right", "2%");
+        add(new DrawerToggle(), title, logoutButton, avatar);
+    }
+}
--- AdvancedVaadin/src/main/java/io/github/raboro/basicvaadin/views/main/MainView.java
+++ AdvancedVaadin/src/main/java/io/github/raboro/basicvaadin/views/main/MainView.java
@@ -1,37 +1,23 @@
 package io.github.raboro.basicvaadin.views.main;
 
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import io.github.raboro.basicvaadin.security.SecurityService;
+import io.github.raboro.basicvaadin.views.header.HeaderView;
 import jakarta.annotation.security.PermitAll;
 
 @PermitAll
 @PageTitle("Main")
 @Route("")
-public class MainView extends HorizontalLayout {
+public class MainView extends AppLayout {
 
     private final SecurityService securityService;
-    private TextField name;
-    private Button sayHello;
 
     public MainView(SecurityService securityService) {
         this.securityService = securityService;
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> Notification.show("Hello " + name.getValue()));
-        sayHello.addClickShortcut(Key.ENTER);
-
-        Button logout = new Button("Logout", e -> this.securityService.logout());
-
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello, logout);
-
-        add(name, sayHello, logout);
+        setPrimarySection(Section.DRAWER);
+        addToNavbar(true, new HeaderView(securityService));
     }
 
 }
