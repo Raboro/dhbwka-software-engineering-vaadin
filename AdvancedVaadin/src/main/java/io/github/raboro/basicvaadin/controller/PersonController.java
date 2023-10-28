fafbce47c794036c332d6ee16df44889771a37f2
Raboro <raborogit@gmail.com>
2023-10-28 16:19:00 +0200

    added basic controller

---
+++ AdvancedVaadin/src/main/java/io/github/raboro/basicvaadin/controller/PersonController.java
@@ -0,0 +1,34 @@
+package io.github.raboro.basicvaadin.controller;
+
+import io.github.raboro.basicvaadin.data.model.Person;
+import io.github.raboro.basicvaadin.data.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+/**
+ * @author MariusWoerfel
+ */
+@Controller
+public class PersonController {
+
+    private final PersonRepository repository;
+
+    @Autowired
+    PersonController(PersonRepository repository) {
+        this.repository = repository;
+        initData();
+    }
+
+    private void initData() {
+        repository.save(new Person("Mike", "Mike@gmail.com", 18, 10));
+        repository.save(new Person("Ralf", "RalleRalf@gmail.com", 22, 10));
+        repository.save(new Person("Ruven", "R.uvenR@gmail.com", 21, 6));
+        repository.save(new Person("Peter", "Peter.P@gmail.com", 18, 5));
+    }
+
+    public List<Person> getAllPersons() {
+        return repository.findAll();
+    }
+}
