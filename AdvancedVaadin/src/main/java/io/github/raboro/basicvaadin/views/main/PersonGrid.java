b209aa3def967fd609df3cbaa5db98e003b3b2f4
Raboro <raborogit@gmail.com>
2023-10-29 12:09:18 +0100

    added delete & edit button

--- AdvancedVaadin/src/main/java/io/github/raboro/basicvaadin/views/main/PersonGrid.java
+++ AdvancedVaadin/src/main/java/io/github/raboro/basicvaadin/views/main/PersonGrid.java
@@ -1,6 +1,13 @@
 package io.github.raboro.basicvaadin.views.main;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import io.github.raboro.basicvaadin.controller.PersonController;
 import io.github.raboro.basicvaadin.data.model.Person;
 
@@ -28,6 +35,22 @@ public class PersonGrid extends Grid<Person> {
         addColumn(Person::getHolidayDays)
                 .setHeader("HolidayDays")
                 .setFooter("Total Holiday Days: " + controller.countHolidayDaysOfAllPersons());
+        addColumn(new ComponentRenderer<>(Span::new, (span, person) -> {
+            // render badge
+        }));
+        addColumn(new ComponentRenderer<>(Button::new, (button, person) -> {
+            button.addThemeVariants(ButtonVariant.LUMO_ICON,
+                    ButtonVariant.LUMO_ERROR,
+                    ButtonVariant.LUMO_TERTIARY);
+            button.addClickListener(e -> Notification.show("Delete"));
+            button.setIcon(new Icon(VaadinIcon.TRASH));
+        })).setHeader("Delete").setWidth("0.5%");
+        addColumn(new ComponentRenderer<>(Button::new, (button, person) -> {
+            button.addThemeVariants(ButtonVariant.LUMO_ICON,
+                    ButtonVariant.LUMO_TERTIARY);
+            button.addClickListener(e -> Notification.show("Edit"));
+            button.setIcon(new Icon(VaadinIcon.EDIT));
+        })).setHeader("Edit").setWidth("0.5%");
     }
 
     public void update() {
