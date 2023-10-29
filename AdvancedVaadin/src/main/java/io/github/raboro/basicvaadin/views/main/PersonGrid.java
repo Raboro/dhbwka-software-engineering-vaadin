9a9f591968a0f5a37761ff50fd00e720fc41d91c
Raboro <raborogit@gmail.com>
2023-10-29 15:41:41 +0100

    removed unused import

--- AdvancedVaadin/src/main/java/io/github/raboro/basicvaadin/views/main/PersonGrid.java
+++ AdvancedVaadin/src/main/java/io/github/raboro/basicvaadin/views/main/PersonGrid.java
@@ -5,7 +5,6 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import io.github.raboro.basicvaadin.controller.PersonController;
 import io.github.raboro.basicvaadin.data.model.Person;
