3d4062a621290d521201eecc3122559a0033bb53
Raboro <raborogit@gmail.com>
2023-10-29 15:53:52 +0100

    added notification on column change

--- AdvancedVaadin/src/main/java/io/github/raboro/basicvaadin/views/main/PersonGrid.java
+++ AdvancedVaadin/src/main/java/io/github/raboro/basicvaadin/views/main/PersonGrid.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import io.github.raboro.basicvaadin.controller.PersonController;
 import io.github.raboro.basicvaadin.data.model.Person;
@@ -91,5 +92,6 @@ public class PersonGrid extends Grid<Person> {
         setItems(this.controller.getAllPersons());
         removeAllColumns();
         constructColumns();
+        Notification.show("Updated Columns", 1000, Notification.Position.TOP_END);
     }
 }
