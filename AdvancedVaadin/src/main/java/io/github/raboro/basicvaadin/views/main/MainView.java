cec7d18a877b052ffe4b8ee1d64a0d3223d2a47e
Raboro <raborogit@gmail.com>
2023-10-29 20:27:56 +0100

    added tabs

--- AdvancedVaadin/src/main/java/io/github/raboro/basicvaadin/views/main/MainView.java
+++ AdvancedVaadin/src/main/java/io/github/raboro/basicvaadin/views/main/MainView.java
@@ -1,13 +1,21 @@
 package io.github.raboro.basicvaadin.views.main;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
 import io.github.raboro.basicvaadin.controller.PersonController;
 import io.github.raboro.basicvaadin.security.SecurityService;
 import io.github.raboro.basicvaadin.views.header.HeaderView;
+import io.github.raboro.basicvaadin.views.login.LoginView;
 import jakarta.annotation.security.PermitAll;
 
 @PermitAll
@@ -16,9 +24,9 @@ import jakarta.annotation.security.PermitAll;
 public class MainView extends AppLayout {
 
     public MainView(SecurityService securityService, PersonController controller) {
-        setPrimarySection(Section.DRAWER);
-        addToNavbar(true, new HeaderView(securityService));
+        addToNavbar(new HeaderView(securityService));
         constructContent(controller);
+        addToDrawer(createTabs());
     }
 
     private void constructContent(PersonController controller) {
@@ -32,6 +40,29 @@ public class MainView extends AppLayout {
         addButton.getStyle().setWidth("10%");
         content.add(addButton);
         setContent(content);
+    }
+
+    private Tabs createTabs() {
+        Tabs tabs = new Tabs();
+        tabs.add(
+                createTab(VaadinIcon.DASHBOARD, "Overview", this),
+                createTab(VaadinIcon.LOCK, "Login", new LoginView())
+        );
+        tabs.setOrientation(Tabs.Orientation.VERTICAL);
+        return tabs;
+    }
+
+    private Tab createTab(VaadinIcon viewIcon, String name, Component clazz) {
+        Icon icon = viewIcon.create();
+        icon.getStyle().set("box-sizing", "border-box")
+                .set("margin-inline-end", "var(--lumo-space-m)")
+                .set("margin-inline-start", "var(--lumo-space-xs)")
+                .set("padding", "var(--lumo-space-xs)");
+        RouterLink link = new RouterLink();
+        link.add(icon, new Span(name));
+        link.setRoute(clazz.getClass());
+        link.setTabIndex(-1);
+        return new Tab(link);
     }
 
 }
